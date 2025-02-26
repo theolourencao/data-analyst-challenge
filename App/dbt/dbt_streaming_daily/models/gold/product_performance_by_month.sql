defe96c7b4138cd4d0029c06ab4e1f5126ed8b02
Theo Lourencao <theo.lourencao@ifood.com.br>
2025-02-26 11:36:02 -0300

    adding power bi file global view; country region files; product aging; store categorization

--- App/dbt/dbt_streaming_daily/models/gold/product_performance_by_month.sql
+++ App/dbt/dbt_streaming_daily/models/gold/product_performance_by_month.sql
@@ -3,18 +3,25 @@
 ) }}
 
 SELECT
-    DATE_TRUNC('month', date) AS month,
-    product,
+    DATE_TRUNC('month', date) AS day,
+    macro_store,
     store,
+    first_payment_date,
     country_code,
     genre_id,
-    SUM(quantity) AS total_quantity,
+    product_aging,
+    is_stream,
+    case 
+        when product_aging = 0 then '0 - early'
+        when product_aging = 1 then '1 - new'
+        when product_aging between 2 and 5 then '2 - existing'
+        else '3 - old' end as product_age_group,
+    SUM(quantity) AS total_streams,
     SUM(revenue) AS total_revenue,
-    SUM(is_stream) AS total_streams,
     SUM(is_download) AS total_downloads
 FROM 
     {{ ref('cleaned_streaming_data') }}
 GROUP BY 
-    month, product, store, country_code, genre_id
+    all
 ORDER BY 
-    month, product, store
+    day, country_code, store
--- App/dbt/dbt_streaming_daily/models/silver/cleaned_streaming_data.sql
+++ App/dbt/dbt_streaming_daily/models/silver/cleaned_streaming_data.sql
@@ -2,7 +2,8 @@
     materialized='table'
 ) }}
 
-SELECT
+with cleansing_data as
+(SELECT
     CAST(store AS STRING) AS store,
     CAST(product AS STRING) AS product,
     CAST(quantity AS INTEGER) AS quantity,
@@ -13,6 +14,16 @@ SELECT
     genre_name,
     date,
     CAST(is_stream AS BOOLEAN) AS is_stream,
-    CAST(is_download AS BOOLEAN) AS is_download
+    CAST(is_download AS BOOLEAN) AS is_download,
+    case when store like 'youtube%' then 'youtube'
+        when store like 'facebook%' then 'facebook'
+        when store like 'spotify%%' then 'spotify'
+        when store like 'tiktok%' then 'tiktok'
+        when store like 'amazon%' then 'amazon' end macro_store,
+    date_trunc('month',min(date) over (partition by product order by date ASC )) first_payment_date
 FROM 
-    {{ ref('streaming_data_view') }}
+    {{ref('streaming_data_view')}}),
+pa as
+(select *, datediff('month',first_payment_date,date_Trunc('month',date)) product_aging
+from cleansing_data cd)
+SELECT * from pa
